b5a23158a219db359e21840115642cf7ab03b5f0
RobertoRodriguezP <rroberto96@hotmail.com>
2020-12-03 21:29:19 -0500

    Seguimiento Jueves

---
+++ app/src/main/java/com/example/proyectodesensores/Barometer.java
@@ -0,0 +1,50 @@
+package com.example.proyectodesensores;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Barometer extends AppCompatActivity {
+
+    private TextView txt;
+    private SensorManager sensorManager;
+    private Sensor pressureSensor;
+    private SensorEventListener sensorEventListener = new SensorEventListener() {
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float[] values = sensorEvent.values;
+            txt.setText(String.format("%.3f mbar", values[0]));
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.barometro);
+        txt = findViewById(R.id.txt);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorEventListener, pressureSensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+}
